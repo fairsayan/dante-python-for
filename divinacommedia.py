7f326d2b23f3fcd701f06ada0764abb41dcc0bff
Domenico Pontari <fairsayan@gmail.com>
2021-04-17 09:26:55 +0200

    with operator

--- divinacommedia.py
+++ divinacommedia.py
@@ -1,16 +1,15 @@
 import operator
-file = open('dante.txt')
-occurences = {}
+occurrences = {}
 
-for line in file:
-	words = line.split(' ')
-	for word in words:
-		word = word.strip()
-		if len(word) <= 3:
-			continue
-		occurences[word] = occurences.get(word, 0) + 1
+with open('dante.txt') as file:
+	for line in file:
+		words = line.split(' ')
+		for word in words:
+			word = word.strip()
+			if len(word) <= 3:
+				continue
+			occurrences[word] = occurrences.get(word, 0) + 1
 
-occurences = sorted( occurences.items(), key=operator.itemgetter(1) )
+	occurrences = sorted( occurrences.items(), key=operator.itemgetter(1), reverse=True )
 
-print(occurences[-20:])
-file.close()
+	print(occurrences[:20])
